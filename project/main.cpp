79a392e7e82eb8a7fd90c1abf84775553d0bc656
Bernard van Gastel <bvgastel@bitpowder.com>
2020-10-10 14:08:04 +0200

    cleaner framework, fixed timing bug

--- project/main.cpp
+++ project/main.cpp
@@ -8,6 +8,7 @@
 #include <sys/time.h>
 
 #include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <mutex>
 #include <thread>
@@ -17,36 +18,34 @@
 using namespace std;
 
 // size of the image
-const int SIZE = 1048577;
+const int64_t SIZE = 524288ULL;
+const int64_t REPEAT = 2ULL;
 
 int main(int argc, char* argv[]) {
 	float* res = new float[SIZE * SIZE];  // result of mean filter
 	float* img = new float[SIZE * SIZE];
 
-	auto start = std::chrono::steady_clock::now();
-
 	// fill with dummy data
-	for (int i = 0; i < SIZE; i++) {
-		for (int j = 0; j < SIZE; j++) {
+	for (int64_t i = 0; i < SIZE; i++) {
+		for (int64_t j = 0; j < SIZE; j++) {
 			img[j * SIZE + i] = (2 * j + i) % 8196;
 		}
 	}
 
 	// do the filter
-	for (int i = 1; i < SIZE - 1; i++) {
-		for (int j = 1; j < SIZE - 1; j++) {
-			res[j * SIZE + i] = 0;
-			for (int k = -1; k < 2; k++) {
-				for (int l = -1; l < 2; l++) {
-					res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+	for (int64_t r = 0; r < REPEAT; ++r) {
+		for (int64_t i = 1; i < SIZE - 1; i++) {
+			for (int64_t j = 1; j < SIZE - 1; j++) {
+				res[j * SIZE + i] = 0;
+				for (int k = -1; k < 2; k++) {
+					for (int l = -1; l < 2; l++) {
+						res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+					}
 				}
+				res[j * SIZE + i] /= 9;
 			}
-			res[j * SIZE + i] /= 9;
 		}
 	}
-	auto finish = std::chrono::steady_clock::now();
-	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
-	std::cout << "image of size " << SIZE << " took " << elapsed.count() << " ms" << std::endl;
 
 	delete[] img;
 	delete[] res;
@@ -54,10 +53,12 @@ int main(int argc, char* argv[]) {
 	struct rusage usage;
 	getrusage(RUSAGE_SELF, &usage);
 
-	std::cout << usage.ru_minflt << std::endl;
-	std::cout << usage.ru_maxrss << std::endl;
-	std::cout << usage.ru_nvcsw << std::endl;
-	std::cout << usage.ru_nivcsw << std::endl;
+	std::cout << "user time:                    " << usage.ru_utime.tv_sec << "." << std::fixed << std::setw(6) << std::setprecision(6) << std::setfill('0') << usage.ru_utime.tv_usec << " s" << std::endl;
+	std::cout << "soft page faults:             " << usage.ru_minflt << std::endl;
+	std::cout << "hard page faults:             " << usage.ru_majflt << std::endl;
+	std::cout << "max memory:                   " << usage.ru_maxrss << " KiB" << std::endl;
+	std::cout << "voluntary context switches:   " << usage.ru_nvcsw << std::endl;
+	std::cout << "involuntary context switches: " << usage.ru_nivcsw << std::endl;
 	// ...
 	return 0;
 }
