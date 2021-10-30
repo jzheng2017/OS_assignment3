a62589bf1b285eddd071bbfc562e0d5eff4e12cd
Sean <55856651+ShenghangWang@users.noreply.github.com>
2021-10-30 22:20:12 +0200

    Add files via upload

--- project/main.cpp
+++ project/main.cpp
@@ -11,7 +11,6 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
 // although it is good habit, you don't have to type 'std::' before many objects by including this line
 using namespace std;
@@ -20,16 +19,12 @@ using namespace std;
 const int64_t SIZE = 16384ULL;
 const int64_t REPEAT = 1ULL;
 
-int main(int argc, char *argv[])
-{
-	float *res = new float[SIZE * SIZE]; // result of mean filter
-	float *img = new float[SIZE * SIZE];
+int main(int argc, char* argv[]) {
+	float* img = new float[SIZE * SIZE];
 
 	// fill with dummy data
-	for (int64_t i = 0; i < SIZE; i++)
-	{
-		for (int64_t j = 0; j < SIZE; j++)
-		{
+	for (int64_t i = 0; i < SIZE; i++) {
+		for (int64_t j = 0; j < SIZE; j++) {
 			img[j * SIZE + i] = (2 * j + i) % 32768;
 		}
 	}
@@ -38,60 +33,35 @@ int main(int argc, char *argv[])
 	uint64_t dummy = 0;
 
 	// ADJUST BELOW, BUT keep writing to the dummy variable
-
-	float res_1[SIZE][SIZE];
+	float* res = new float[SIZE * SIZE];  // result of mean filter
 
 	// Apply an averaging imaging filter to some input image, and write in to an output image.
 	// A pixel in the output image is calculated by averaging 9 pixels: the pixel at the same
 	// coordinates in the input image, and the adjecent pixels.
+	for (int64_t r = 0; r < REPEAT; ++r) {
+		for (int64_t j = 1; j < SIZE - 1; j++) {
+			for (int64_t i = 1; i < SIZE - 1; i++) {
+				res[j * SIZE + i] = 0;
+				for (long k = -1; k < 2; k++) {
+					for (long l = -1; l < 2; l++) {
+						res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+					}
+				}
+				res[j * SIZE + i] /= 9;
+			}
+		}
 
-	for (int64_t r = 0; r < REPEAT; ++r)
-	{
-		for (int64_t i = 1; i < SIZE - 1; i++)
-		{
-			for (int64_t j = 1; j < SIZE - 1; j++)
-			{
-				float tmp = 0;
-
-				tmp += img[(j - 1) * SIZE + i - 1];
-				tmp += img[j * SIZE + i - 1];
-				tmp += img[(j + 1) * SIZE + i - 1];
-				tmp += img[(j - 1) * SIZE + i];
-				tmp += img[j * SIZE + i];
-				tmp += img[(j + 1) * SIZE + i];
-				tmp += img[(j - 1) * SIZE + i + 1];
-				tmp += img[j * SIZE + i + 1];
-				tmp += img[(j + 1) * SIZE + i + 1];
-
-				tmp /= 9;
-				res_1[j-1][i-1] = tmp;
-				dummy += tmp;
+		for (int64_t j = 1; j < SIZE - 1; j++) {
+			for (int64_t i = 1; i < SIZE - 1; i++) {
+				dummy += res[j * SIZE + i];
 			}
 		}
 	}
-// for (int64_t r = 0; r < REPEAT; ++r) {
-// 		for (int64_t i = 1; i < SIZE - 1; i++) {
-// 			for (int64_t j = 1; j < SIZE - 1; j++) {
-// 				res[j * SIZE + i] = 0;
-// 				for (long k = -1; k < 2; k++) {
-// 					for (long l = -1; l < 2; l++) {
-// 						res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
-// 					}
-// 				}
-// 				res[j * SIZE + i] /= 9;
-// 			}
-// 		}
 
-// 		for (int64_t i = 1; i < SIZE - 1; i++) {
-// 			for (int64_t j = 1; j < SIZE - 1; j++) {
-// 				dummy += res[j * SIZE + i];
-// 			}
-// 		}
-// 	}
-	// ADJUST ABOVE, BUT keep writing to the dummy variable
-
-	delete[] img;
 	delete[] res;
+
+	// ADJUST ABOVE, BUT keep writing to the dummy variable
+	delete[] img;
 
 	struct rusage usage;
 	getrusage(RUSAGE_SELF, &usage);
@@ -100,7 +70,7 @@ int main(int argc, char *argv[])
 	std::cout << "soft page faults:             " << usage.ru_minflt << std::endl;
 	std::cout << "hard page faults:             " << usage.ru_majflt << std::endl;
 #ifdef __APPLE__
-	std::cout << "max memory:                   " << usage.ru_maxrss / 1024 << " KiB" << std::endl;
+	std::cout << "max memory:                   " << usage.ru_maxrss/1024 << " KiB" << std::endl;
 #else
 	std::cout << "max memory:                   " << usage.ru_maxrss << " KiB" << std::endl;
 #endif
